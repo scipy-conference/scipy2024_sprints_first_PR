e279ca78c99f06bfef89af3cc7a2d59b251ceea2
Henry Cuzco <40706933+uiucmeche1317@users.noreply.github.com>
2024-07-12 17:02:00 -0700

    Added heaviside step function to simple_functions.py

--- participants/scipy_000/pr_tutorial/simple_functions.py
+++ participants/scipy_000/pr_tutorial/simple_functions.py
@@ -10,3 +10,9 @@ def factorial(value):
         return 1
     else:
         return value * factorial(value - 1)
+
+def heaviside(value):
+    if value >= 0:
+        return 1
+    else:
+        return 0
