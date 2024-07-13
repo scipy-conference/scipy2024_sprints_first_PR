28ba03e6f3cefda0b4638036f78d210fb0d3a976
John Drake <drakejh@gmail.com>
2024-07-12 17:03:41 -0700

    May have copied stackoverflow for a function... maybe

--- participants/scipy_038/pr_tutorial/simple_functions.py
+++ participants/scipy_038/pr_tutorial/simple_functions.py
@@ -10,3 +10,19 @@ def factorial(value):
         return 1
     else:
         return value * factorial(value - 1)
+
+def is_prime(number):
+    # This may have been stolen from stack overflow
+    is_prime = True
+
+    for i in range(2, int(user_input)):
+        if int(user_input) % i == 0 :
+            is_prime = False
+            break #break out of the for loop since the number isn't prime
+
+    if is_prime:
+        print("Your number is prime")
+    else:
+        print("your number is not prime")
+	
+
