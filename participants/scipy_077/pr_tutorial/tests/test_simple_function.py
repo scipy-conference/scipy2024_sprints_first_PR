c8eb33bf583eca67ce81c6a66267bb3dfac85a3e
Alex Crosby <crosbyar@gmail.com>
2024-07-12 17:06:49 -0700

    added comment describing the assert statement in the test_factorial_3 test

--- participants/scipy_077/pr_tutorial/tests/test_simple_function.py
+++ participants/scipy_077/pr_tutorial/tests/test_simple_function.py
@@ -4,4 +4,5 @@ from pr_tutorial.simple_functions import factorial
 def test_factorial_3():
     """Simplest test for one crete case"""
 
+    # Fail and throw error if not !3==6
     assert factorial(3) == 6
