fbf16175db04264e79772688b6ab1e8c62ef7d94
Ethan Fricker, PhD <eefricker@gmail.com>
2024-07-12 17:15:04 -0700

    Wrote is_prime()
    Wrote the is_prime function in simple_function file. Did not update the readme or the buggy function.

--- participants/scipy_064/pr_tutorial/simple_functions.py
+++ participants/scipy_064/pr_tutorial/simple_functions.py
@@ -10,3 +10,15 @@ def factorial(value):
         return 1
     else:
         return value * factorial(value - 1)
+    
+def is_prime(intNumber):
+    
+    if type(i) != int:
+        raise OSError("Need to pass int")
+    if i <= 1:
+        raise OSError("Need to pass integer > 1")
+    
+    for i in range(1,intNumber):
+        if (intNumber % i) == 0:
+            return True
+    return False
