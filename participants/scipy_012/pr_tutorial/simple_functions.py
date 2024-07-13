905494be81ff06a649c258772246e8417f871bf9
Tyler <tyler.hopkin12@gmail.com>
2024-07-12 17:13:23 -0700

    Added is_prime function

--- participants/scipy_012/pr_tutorial/simple_functions.py
+++ participants/scipy_012/pr_tutorial/simple_functions.py
@@ -10,3 +10,9 @@ def factorial(value):
         return 1
     else:
         return value * factorial(value - 1)
+    
+def is_prime(value):
+    if value %2 == 0 or value == 0:
+        return False
+    else:
+        return True    
